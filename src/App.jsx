4fc22a0b19817f0087577d48a6bd2e0ddeca3aae
jcollante2021 <jcollante@outlook.com.ar>
2021-12-29 16:50:04 -0300

    Creacion de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,44 @@
+import { useState } from "react";
 import "./app.css"
+import Quiz from "./components/Quiz.jsx";
 
 function App() {
+  const [questionNumber, setQuestionNumber] = useState(1)
+  const moneyPiramid = [
+    { id: 1, amount: "$ 100" },
+    { id: 2, amount: "$ 200" },
+    { id: 3, amount: "$ 300" },
+    { id: 4, amount: "$ 500" },
+    { id: 5, amount: "$ 1.000" },
+    { id: 6, amount: "$ 2.000" },
+    { id: 7, amount: "$ 4.000" },
+    { id: 8, amount: "$ 8.000" },
+    { id: 9, amount: "$ 16.000" },
+    { id: 10, amount: "$ 32.000" },
+    { id: 11, amount: "$ 64.000" },
+    { id: 12, amount: "$ 125.000" },
+    { id: 13, amount: "$ 250.000" },
+    { id: 14, amount: "$ 500.000" },
+    { id: 15, amount: "$ 1.000.000" },
+  ].reverse();
   return (
     <div className="app">
-      <div className="main">Main</div>
+      <div className="main">
+        <div className="top">
+          <div className="timer">30</div>
+        </div>
+        <div className="bottom">
+          <Quiz/>
+        </div>
+      </div>
       <div className="piramid">
         <ul className="moneyList">
-          <li className="moneyListItem">
-            <span className="moneyListItemNumber">1</span>
-            <span className="moneyListItemMount">$ 100</span>
-          </li>
-          <li className="moneyListItem">
-            <span className="moneyListItemNumber">1</span>
-            <span className="moneyListItemMount">$ 100</span>
-          </li>
-          <li className="moneyListItem">
-            <span className="moneyListItemNumber">1</span>
-            <span className="moneyListItemMount">$ 100</span>
-          </li>
-          <li className="moneyListItem">
-            <span className="moneyListItemNumber">1</span>
-            <span className="moneyListItemMount">$ 100</span>
-          </li>
-          
+          {moneyPiramid.map((m) =>(
+            <li className={questionNumber == m.id ? "moneyListItem active" : "moneyListItem"}>
+              <span className="moneyListItemNumber">{m.id}</span>
+              <span className="moneyListItemAmount">{m.amount}</span>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,15 @@
+
+
+export default function quiz() {
+    return (
+        <div className="trivia">
+            <div className="question">¿Cual es la primera pregunta que va a aparecer?</div>
+            <div className="answers">
+                <div className="answer">Opcion A</div>
+                <div className="answer">Opcion B</div>
+                <div className="answer">Opcion C</div>
+                <div className="answer">Opcion D</div>
+            </div>
+        </div>
+    )
+}
