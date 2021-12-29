b9729f7d18035f26bc7dfe7a994ac3c301c42d5e
jcollante2021 <jcollante@outlook.com.ar>
2021-12-29 15:58:48 -0300

    Creando Quiz App Esqueleto

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import "./app.css"
+
+function App() {
+  return (
+    <div className="app">
+      <div className="main">Main</div>
+      <div className="piramid">
+        <ul className="moneyList">
+          <li className="moneyListItem">
+            <span className="moneyListItemNumber">1</span>
+            <span className="moneyListItemMount">$ 100</span>
+          </li>
+          <li className="moneyListItem">
+            <span className="moneyListItemNumber">1</span>
+            <span className="moneyListItemMount">$ 100</span>
+          </li>
+          <li className="moneyListItem">
+            <span className="moneyListItemNumber">1</span>
+            <span className="moneyListItemMount">$ 100</span>
+          </li>
+          <li className="moneyListItem">
+            <span className="moneyListItemNumber">1</span>
+            <span className="moneyListItemMount">$ 100</span>
+          </li>
+          
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
